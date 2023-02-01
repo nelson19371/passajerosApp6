e0730780e799671e0fdf4a2788a2e8bc22c36bb2
@nelsondev <50004176+nelson19371@users.noreply.github.com>
2023-02-01 06:32:03 -0300

    Update pasajerosPage.jsx

--- src/pasajeros/page/pasajerosPage.jsx
+++ src/pasajeros/page/pasajerosPage.jsx
@@ -70,7 +70,7 @@ export const PasajerosPage = () => {
       }
       
       let settings = {
-        fileName: "Listado Pasajeros Mina", // Name of the resulting spreadsheet
+        fileName: "Listado Pasajeros Turno Planta", // Name of the resulting spreadsheet
         extraLength: 3, // A bigger number means that columns will be wider
         writeMode: "writeFile", // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
         writeOptions: {}, // Style options from https://docs.sheetjs.com/docs/api/write-options
