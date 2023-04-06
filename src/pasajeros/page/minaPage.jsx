b34a3a82bb3edf1e2cfd6c84503d6b513b508fb0
@nelsondev <50004176+nelson19371@users.noreply.github.com>
2023-04-06 15:44:58 -0400

    Update minaPage.jsx

--- src/pasajeros/page/minaPage.jsx
+++ src/pasajeros/page/minaPage.jsx
@@ -8,7 +8,7 @@ export const MinaPage = () => {
   const [search, setSearch] = useState("")
   let jsonObject =''
   //funcion para traer los datos de la API
-  const URL = 'https://api3-production.up.railway.app/api/employees'
+  const URL = 'https://api3-production.up.railway.app/api/minas'
 
   const showData = async() =>{
      axios.get(URL).then((res) =>{
